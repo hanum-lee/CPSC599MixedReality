ba8fb05ccc9b6e8f681da6bc496f97371dea736c
hanum-lee <chococloudy@gmail.com>
2020-02-14 02:59:02 -0700

    Assignment 1 done

--- Assignment 1/Assets/CustomScripts/Player.cs
+++ Assignment 1/Assets/CustomScripts/Player.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player : DefaultTrackableEventHandler
 {
     // Start is called before the first frame update
 
@@ -11,16 +11,34 @@ public class Player : MonoBehaviour
     private GameObject target;
     private Rigidbody rigid;
     private int position;
+    Vector3 originPosition;
+    bool found = false;
 
-    
+
     void Start()
     {
         anima = GetComponent<Animator>();
         target = GameObject.Find("Target");
         rigid = GetComponent<Rigidbody>();
         position = 0;
+        
+        Vector3 originPosition = transform.position;
 
     }
+
+    protected override void OnTrackingFound()
+    {
+        base.OnTrackingFound();
+        found = true;
+    }
+
+    protected override void OnTrackingLost()
+    {
+        base.OnTrackingLost();
+        found = false;
+
+    }
+
 
     // Update is called once per frame
     void Update()
@@ -28,16 +46,18 @@ public class Player : MonoBehaviour
         
         if(position == 0)
         {
-            if (gameObject.transform.position.z > -2)
+            Debug.Log(gameObject.transform.position.z);
+            if (gameObject.transform.position.z > -0.25)
             {
                 aniplaying = true;
-                gameObject.transform.Translate(Vector3.forward * Time.deltaTime);
+                gameObject.transform.position = (Vector3.MoveTowards(gameObject.transform.position,new Vector3(originPosition.x,originPosition.y,-2 ), Time.deltaTime * 0.05f));
+
                 //anima.Play("WALK00_F", -1,0.0f);
             }
             else
             {
                 aniplaying = false;
-                //Debug.Log("stoped");
+                Debug.Log("stoped");
                 //anima.Play("POSE_02", -1, 0.0f);
                 position = 1;
 
@@ -46,14 +66,50 @@ public class Player : MonoBehaviour
         }
         else if(position == 1)
         {
-            if (gameObject.transform.position.x > 2)
+            if(gameObject.transform.rotation.y > 90)
             {
+                gameObject.transform.Rotate(Vector3.left * Time.deltaTime);
+            }
+            else
+            {
+                if (gameObject.transform.position.x < 0.2)
+                {
+                    aniplaying = true;
+                    gameObject.transform.Translate(Vector3.left * Time.deltaTime * 0.05f);
+                }
+                else
+                {
+                    aniplaying = false;
+                    //anima.Play("POSE_02", -1, .0f);
+                    position = 2;
+                }
+                anima.SetBool("isWalkingL", aniplaying);
+            }
+
+        }
+        else if (position == 2)
+        {
+            
+            
+            
+            
+            if (Vector3.Distance(gameObject.transform.position, originPosition) <= 0.001f)
+            {
+
+                anima.SetBool("IsWalking", false);
+                //gameObject.transform.position = target.transform.position;
+                //target.SetActive(false);
+                anima.Play("DAMAGED01", -1, .0f);
+                position = 3;
 
             }
             else
             {
-
+                gameObject.transform.position = (Vector3.MoveTowards(gameObject.transform.position, originPosition, Time.deltaTime * 0.05f));
+                anima.SetBool("IsWalking", true);
             }
+            
+            
         }
         
         
@@ -61,13 +117,15 @@ public class Player : MonoBehaviour
         
     }
 
+    
+
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.name == "Target")
         {
             Debug.Log("cube hit");
             anima.SetBool("IsWalking", false);
-            anima.Play("DAMAGED00", -1, .0f);
+            anima.Play("DAMAGED01", -1, .0f);
         }
     }
 
---
+++ Assignment 1/Assets/CustomScripts/TestingScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestingScript : DefaultTrackableEventHandler
+{
+    // Start is called before the first frame update
+    public GameObject uni;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    
+
+    protected override void OnTrackingFound()
+    {
+        base.OnTrackingFound();
+        this.transform.Translate(Vector3.forward * Time.deltaTime * 0.05f);
+
+    }
+
+    protected override void OnTrackingLost()
+    {
+        base.OnTrackingLost();
+
+    }
+
+
+}
